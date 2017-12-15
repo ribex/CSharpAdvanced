6f01d9a161c03d5f0e89abba16bfb9c5bfaf6a33
ribex <ribexy@gmail.com>
2017-12-14 20:15:55 -0500

    Complete Generics video

--- Generics/Generics/Utilities.cs
+++ Generics/Generics/Utilities.cs
@@ -2,11 +2,21 @@
 
 namespace Generics
 {
-    public class Utilities<T> where T : IComparable
+    // where T : IComparable
+    // where T : Product
+    // where T : struct
+    // where T : class
+    // where T : new() <---- (default constructor)
+    public class Utilities<T> where T : IComparable, new()
     {
         public int Max(int a, int b)
         {
             return a > b ? a : b;
+        }
+
+        public void DoSomething(T value)
+        {
+            var obj = new T();
         }
 
         // generic method, requires constraint of IComparable
