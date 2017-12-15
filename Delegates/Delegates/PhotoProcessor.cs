10e359f13bcb91c7e3f22bf6f3b4476e5d1f3b37
ribex <ribexy@gmail.com>
2017-12-14 20:59:55 -0500

    Use generic Action delegate

--- Delegates/Delegates/PhotoProcessor.cs
+++ Delegates/Delegates/PhotoProcessor.cs
@@ -1,15 +1,16 @@
-﻿namespace Delegates
+﻿using System;
+
+namespace Delegates
 {
     public class PhotoProcessor
     {
-        public void Process(string path)
+        public delegate void PhotoFilterHandler(Photo photo);
+
+        public void Process(string path, Action<Photo> filterHandler)
         {
             var photo = Photo.Load(path);
 
-            var filters = new PhotoFilters();
-            filters.ApplyBrightness(photo);
-            filters.ApplyContrast(photo);
-            filters.Resize(photo);
+            filterHandler(photo);
 
             photo.Save();
         }
