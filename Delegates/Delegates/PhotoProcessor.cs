ca07f537f582deb422ef1f6c207de110e1a16ca8
ribex <ribexy@gmail.com>
2017-12-14 20:39:21 -0500

    Begin example of delegates

---
+++ Delegates/Delegates/PhotoProcessor.cs
@@ -0,0 +1,17 @@
+﻿namespace Delegates
+{
+    public class PhotoProcessor
+    {
+        public void Process(string path)
+        {
+            var photo = Photo.Load(path);
+
+            var filters = new PhotoFilters();
+            filters.ApplyBrightness(photo);
+            filters.ApplyContrast(photo);
+            filters.Resize(photo);
+
+            photo.Save();
+        }
+    }
+}
