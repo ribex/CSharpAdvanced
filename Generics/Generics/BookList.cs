cb41d98aad9abf5d567575cb6c5c8555290360f2
ribex <ribexy@gmail.com>
2017-12-14 19:20:51 -0500

    Comparing boxed objects

---
+++ Generics/Generics/BookList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Generics
+{
+    public class BookList
+    {
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+
+    }
+
+    public class ObjectList
+    {
+        public void Add(object value)
+        {
+            
+        }
+
+        public object this[int index]
+        {
+            get { throw new NotImplementedException();}
+        }
+    }
+
+}
