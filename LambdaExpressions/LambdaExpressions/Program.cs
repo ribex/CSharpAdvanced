a907f7570b5a45626814c03f82d218c978694dfe
ribex <ribexy@gmail.com>
2017-12-16 10:51:28 -0500

    Complete Lambda Expressions examples

--- LambdaExpressions/LambdaExpressions/Program.cs
+++ LambdaExpressions/LambdaExpressions/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +9,40 @@ namespace LambdaExpressions
     {
         static void Main(string[] args)
         {
+
+            // args => expression
+
+            // () => ...
+            // x => ...
+            // (x, y, z) => ...
+
+            Func<int, int> square = number => number * number;
+
+            Console.WriteLine(square(5));   // 25
+
+
+            const int factor = 5;
+            Func<int, int> multiplier = n => n * factor;
+
+            var result = multiplier(10);
+
+            Console.WriteLine(result); // 50
+
+            var books = new BookRepository().GetBooks();
+
+//            var cheapBooks = books.FindAll(IsCheaperThan10Dollars);
+            var cheapBooks = books.FindAll(book => book.Price < 10);
+
+            foreach (var book in cheapBooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+            
         }
+
+//        static bool IsCheaperThan10Dollars(Book book)
+//        {
+//            return book.Price < 10;
+//        }
     }
 }
