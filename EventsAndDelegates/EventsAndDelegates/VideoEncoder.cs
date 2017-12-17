6bd66d0e6ea73d9097dad2a1b43696305d6020d7
ribex <ribexy@gmail.com>
2017-12-16 23:17:30 -0500

    Create delegate and event handler in VideoEncoder example

--- EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
+++ EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
@@ -5,10 +5,28 @@ namespace EventsAndDelegates
 {
     public class VideoEncoder
     {
+        // 1) Define a delegate
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);  
+
+        // 2) Define an event based on that delegate
+        public event VideoEncodedEventHandler VideoEncoded;
+
         public void Encode(Video video)
         {
             Console.WriteLine("Encoding video...");
             Thread.Sleep(3000);
+
+            // 3) Raise the event
+            OnVideoEncoded();
+        }
+        
+        // (part of 3)
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncoded != null)
+            {
+                VideoEncoded(this, EventArgs.Empty);
+            }
         }
     }
 }
