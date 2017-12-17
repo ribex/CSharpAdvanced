d14c89838233d4637123049bf23f2285a44bd012
ribex <ribexy@gmail.com>
2017-12-17 00:11:49 -0500

    Pass data about events that have happened with VideoEventsArgs

--- EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
+++ EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
@@ -3,10 +3,15 @@ using System.Threading;
 
 namespace EventsAndDelegates
 {
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }    
+    }
+
     public class VideoEncoder
     {
         // 1) Define a delegate
-        public delegate void VideoEncodedEventHandler(object source, EventArgs args);  
+        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);  
 
         // 2) Define an event based on that delegate
         public event VideoEncodedEventHandler VideoEncoded;
@@ -17,15 +22,15 @@ namespace EventsAndDelegates
             Thread.Sleep(3000);
 
             // 3) Raise the event
-            OnVideoEncoded();
+            OnVideoEncoded(video);
         }
         
         // (part of 3)
-        protected virtual void OnVideoEncoded()
+        protected virtual void OnVideoEncoded(Video video)
         {
             if (VideoEncoded != null)
             {
-                VideoEncoded(this, EventArgs.Empty);
+                VideoEncoded(this, new VideoEventArgs(){Video = video});
             }
         }
 
