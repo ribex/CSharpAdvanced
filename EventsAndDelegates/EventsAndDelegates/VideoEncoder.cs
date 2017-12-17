4c3beef9978286ca9c823b3f87419131d3ccc28f
ribex <ribexy@gmail.com>
2017-12-17 00:17:18 -0500

    Refactor delegate using build in delegate

--- EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
+++ EventsAndDelegates/EventsAndDelegates/VideoEncoder.cs
@@ -11,10 +11,12 @@ namespace EventsAndDelegates
     public class VideoEncoder
     {
         // 1) Define a delegate
-        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);  
+        // public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);  
 
         // 2) Define an event based on that delegate
-        public event VideoEncodedEventHandler VideoEncoded;
+        public event EventHandler<VideoEventArgs> VideoEncoded;
+
+        // public event EventHandler VideoEncoded;
 
         public void Encode(Video video)
         {
