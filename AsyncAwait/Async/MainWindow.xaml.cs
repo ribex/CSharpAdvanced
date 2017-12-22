6b19bb4585e9671bfbaacdedde6da78d1e7df436
ribex <ribexy@gmail.com>
2017-12-21 19:13:17 -0500

    Async await

---
+++ AsyncAwait/Async/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Net;
+using System.Windows;
+
+namespace Async
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadHtml("https://msdn.microsoft.com");
+        }
+
+        public 
+
+        public void DownloadHtml(string url)
+        {
+            var webClient = new WebClient();
+            var html = webClient.DownloadString(url);
+
+            using (var streamWriter = new StreamWriter(@"c:\projects\result.html"))
+            {
+                streamWriter.Write(html);
+            }
+        }
+    }
+}
